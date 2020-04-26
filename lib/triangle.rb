4b973d64e5e2ed006521ff85c31b9960d77d3fbc
proactive-analyst-6157 <u.batsaikhan@gmail.com>
2020-04-26 13:57:58 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -16,17 +16,23 @@ class Triangle
             @side_1 + @side_3 > @side_2) &&
                 (@side_1 < 0 || @side_2 < 0 || @side_3 < 0)
       :equilateral
-    elsif (@side_1 == @side_2 && @side_1 != @side_3 && @side_2 != @side_3) || 
+    elsif ((@side_1 == @side_2 && @side_1 != @side_3 && @side_2 != @side_3) || 
           (@side_1 == @side_3 && @side_1 != @side_2 && @side_2 != @side_3) ||
-          (@side_3 == @side_2 && @side_1 != @side_3 && @side_1 != @side_2)
+          (@side_3 == @side_2 && @side_1 != @side_3 && @side_1 != @side_2)) &&
+                 !(@side_1 == 0 || @side == 0 || @side_3 == 0) &&
+            (@side_1 + @side_2 > @side_3 && 
+            @side_2 + @side_3 > @side_1 && 
+            @side_1 + @side_3 > @side_2) &&
+                (@side_1 < 0 || @side_2 < 0 || @side_3 < 0)
       :isosceles
-    elsif @side_1 != @side_2 && @side_2 != @side_3 && @side_1 != @side_3 
+    elsif (@side_1 != @side_2 && @side_2 != @side_3 && @side_1 != @side_3)
+           !(@side_1 == 0 || @side == 0 || @side_3 == 0) &&
+            (@side_1 + @side_2 > @side_3 && 
+            @side_2 + @side_3 > @side_1 && 
+            @side_1 + @side_3 > @side_2) &&
+                (@side_1 < 0 || @side_2 < 0 || @side_3 < 0)
       :scalene
-
-
-    elsif 
-
-    elsif )
+    else
       begin 
         raise TriangleError
       rescue TriangleError => error
