fde95ac3b5e8f8a990ff3a12b9dba53dfe3f8280
proactive-analyst-6157 <u.batsaikhan@gmail.com>
2020-04-26 14:12:49 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -18,7 +18,7 @@ class Triangle
             (@side_1 + @side_2 > @side_3 && 
             @side_2 + @side_3 > @side_1 && 
             @side_1 + @side_3 > @side_2) &&
-                (@side_1 < 0 && @side_2 < 0 && @side_3 < 0)
+                !(@side_1 < 0 && @side_2 < 0 && @side_3 < 0)
       :isosceles
     elsif (@side_1 != @side_2 && @side_2 != @side_3 && @side_1 != @side_3)
            !(@side_1 == 0 || @side == 0 || @side_3 == 0) &&
