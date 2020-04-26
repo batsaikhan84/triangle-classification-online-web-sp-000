a96e429550533b28aadbf48a3907877d6c0c35f2
proactive-analyst-6157 <u.batsaikhan@gmail.com>
2020-04-26 13:56:52 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,7 +10,11 @@ class Triangle
   
   def kind
     if @side_1 == @side_2 && @side_2 == @side_3 && @side_1 == @side_3 && 
-       !(@side_1 == 0 || @side == 0 || @side_3 == 0)
+       !(@side_1 == 0 || @side == 0 || @side_3 == 0) &&
+            (@side_1 + @side_2 > @side_3 && 
+            @side_2 + @side_3 > @side_1 && 
+            @side_1 + @side_3 > @side_2) &&
+                (@side_1 < 0 || @side_2 < 0 || @side_3 < 0)
       :equilateral
     elsif (@side_1 == @side_2 && @side_1 != @side_3 && @side_2 != @side_3) || 
           (@side_1 == @side_3 && @side_1 != @side_2 && @side_2 != @side_3) ||
@@ -18,11 +22,11 @@ class Triangle
       :isosceles
     elsif @side_1 != @side_2 && @side_2 != @side_3 && @side_1 != @side_3 
       :scalene
-    elsif @side_1 == 0 && @side_2 == 0 && @side_3 == 0
 
-    elsif @side_1 < 0 || @side_2 < 0 || @side_3 < 0
 
-    elsif !(@side_1 + @side_2 > @side_3 && @side_2 + @side_3 > @side_1 && @side_1 + @side_3 > @side_2)
+    elsif 
+
+    elsif )
       begin 
         raise TriangleError
       rescue TriangleError => error
